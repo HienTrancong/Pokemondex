7a9f3ec752d67f0da6c703ab530400ef360b8a7a
HienTrancong <89924593+HienTrancong@users.noreply.github.com>
2021-10-24 17:09:29 +0200

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+alert('Hello world');﻿
+let myName='Hien';
+document.write(myName);
+myName = 'Hien Tran';
+document.write(myName);
+let simpleAddition=2+2;
+document.write(simpleAddition);
+let size =100;
+let doubleSize=size*2;
+document.write(doubleSize);
+let minSize= (doubleSize*2) - (size/2);
+document.write(minSize);
