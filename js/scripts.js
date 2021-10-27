ea579db6a8c304abdb08f6bcddffe73bb19121ed
HienTrancong <89924593+HienTrancong@users.noreply.github.com>
2021-10-28 00:02:04 +0200

    Create Pokemon list

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,6 @@
-alert('Hello world');﻿
-let favoriteFood = 'Pho';
-document.write(favoriteFood);
+let pokemonList = [
+  {name : 'Bulbasaur', height : 0.7, type : ['grass','poison'], ability : ['chlorophyll','overgrow']},
+  {name : 'Charmander', height : 0.6, type : ['fire'], ability : ['blaze','solar-power']},
+  {name : 'Squirtle', height : 0.5, type : ['water'], ability : ['rain-dish','torrent']},
+  {name : 'Butterfree', height : 1.1, type : ['bug','flying'], ability : ['compoundeyes','tinted-lens']}
+];
