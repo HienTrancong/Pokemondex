a06f600b194a3a946de958dffb6c69215beb39a8
HienTrancong <89924593+HienTrancong@users.noreply.github.com>
2021-11-01 23:36:41 +0100

    create pokemon list

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,10 @@ let pokemonList = [
   {name : 'Squirtle', height : 0.5, type : ['water'], ability : ['rain-dish','torrent']},
   {name : 'Butterfree', height : 1.1, type : ['bug','flying'], ability : ['compoundeyes','tinted-lens']}
 ];
+
+
+for ( let i = 0; i < pokemonList.length; i++)
+  {
+    if (pokemonList[i].height >= 1) {document.write(pokemonList[i].name + ' '+'(height: '+pokemonList[i].height+') '+'Wow, that\'s a big pokemon!'+ '<br>');}
+    else {document.write(pokemonList[i].name + ' '+'(height: '+pokemonList[i].height+')' +'<br>');}
+  }
