c0e5f229e3ab09eea6be4f62bdb8e5866216d6f5
HienTrancong <89924593+HienTrancong@users.noreply.github.com>
2021-11-13 22:49:47 +0100

    use IIFE to store pokemon list & forEach to call

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,47 @@
 
-// create simple pokemon list
-  let pokemonList = [
-    {name : 'Bulbasaur', height : 0.7, type : ['grass','poison'], ability : ['chlorophyll','overgrow']},
-    {name : 'Charmander', height : 0.6, type : ['fire'], ability : ['blaze','solar-power']},
-    {name : 'Squirtle', height : 0.5, type : ['water'], ability : ['rain-dish','torrent']},
-    {name : 'Butterfree', height : 1.1, type : ['bug','flying'], ability : ['compoundeyes','tinted-lens']}
-  ];
+// create pokemon list
+let pokemonRepository = ( // pokemon Repository variable to hold IIFE what returns
+  function() { //IIFE function to store pokemon list
+    let pokemonList = [
+      {name : 'Bulbasaur', height : 0.7, type : ['grass','poison'], ability : ['chlorophyll','overgrow']},
+      {name : 'Charmander', height : 0.6, type : ['fire'], ability : ['blaze','solar-power']},
+      {name : 'Squirtle', height : 0.5, type : ['water'], ability : ['rain-dish','torrent']},
+      {name : 'Butterfree', height : 1.1, type : ['bug','flying'], ability : ['compoundeyes','tinted-lens']}
+    ];
+    function getAll() {return pokemonList;}
+    function add(pokemon) {
+      if
+        (typeof(pokemon) !== 'object') // condition to filter only input with object data type
+        {alert('This is not object type');} //statement alert if not object type
+      else if
+        ((Object.keys(pokemon)[0] != 'name') || (Object.keys(pokemon)[1] != 'height') || (Object.keys(pokemon)[2] != 'type') || (Object.keys(pokemon)[3] != 'ability')) // condition to filter only correct keys
+        {alert('Not correct keys');} //statement alert if incorrect keys
+      else
+        (pokemonList.push(pokemon)); //statement to add new pokemon item
+    }
+    return {
+      getAll: getAll,
+      add: add
+    }
+  }
+)();
 
+// function calling each polemon in array using forEach
+function getPokemonList(pokemon) {
+  document.write (
+    '<p>'
+    + 'Name: ' + pokemon.name + '</br>'
+    + 'Height: ' + pokemon.height + '</br>'
+    + 'Type: ' + pokemon.type + '</br>'
+    + 'Ability: ' + pokemon.ability + '</br>'
+    + '</p>'
+  );
+}
+
+pokemonRepository.getAll().forEach(getPokemonList);
+
+//------------ARCHIVE---------------------
+// pokemonRepository.add({name : 'test', height : 0.6, type : ['fire'], ability : ['blaze','solar-power']})
 // calling the pokemon names and heights
   // for ( let i = 0; i < pokemonList.length; i++)
   //   {
@@ -14,18 +49,3 @@
   //     if (pokemonList[i].height >= 1) {document.write('<p>'+pokemonList[i].name +' '+'(height: '+pokemonList[i].height+') '+'Wow, that\'s a big pokemon!'+'</p>');}
   //     else {document.write('<p>'+pokemonList[i].name + ' '+'(height: '+pokemonList[i].height+')'+'</p>');}
   //   }
-
-// calling all pokemon using forEach
-
-function getPokemonList(pokemon)
-  {
-    document.write(
-      '<p>'
-      + pokemon.name + '</br>'
-      + 'Height: ' + pokemon.height + '</br>'
-      + 'Type: ' + pokemon.type + '</br>'
-      + 'Ability: ' + pokemon.ability + '</br>'
-      +'</p>');
-  }
-
-pokemonList.forEach(getPokemonList);
