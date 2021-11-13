b8c4cb4f9373d9cce48621fe7ad2f6d78656357d
HienTrancong <89924593+HienTrancong@users.noreply.github.com>
2021-11-13 13:15:28 +0100

    refactor calling pokemon list
    using forEach

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,31 @@
-let pokemonList = [
-  {name : 'Bulbasaur', height : 0.7, type : ['grass','poison'], ability : ['chlorophyll','overgrow']},
-  {name : 'Charmander', height : 0.6, type : ['fire'], ability : ['blaze','solar-power']},
-  {name : 'Squirtle', height : 0.5, type : ['water'], ability : ['rain-dish','torrent']},
-  {name : 'Butterfree', height : 1.1, type : ['bug','flying'], ability : ['compoundeyes','tinted-lens']}
-];
 
+// create simple pokemon list
+  let pokemonList = [
+    {name : 'Bulbasaur', height : 0.7, type : ['grass','poison'], ability : ['chlorophyll','overgrow']},
+    {name : 'Charmander', height : 0.6, type : ['fire'], ability : ['blaze','solar-power']},
+    {name : 'Squirtle', height : 0.5, type : ['water'], ability : ['rain-dish','torrent']},
+    {name : 'Butterfree', height : 1.1, type : ['bug','flying'], ability : ['compoundeyes','tinted-lens']}
+  ];
 
-for ( let i = 0; i < pokemonList.length; i++)
+// calling the pokemon names and heights
+  // for ( let i = 0; i < pokemonList.length; i++)
+  //   {
+  //     // mark a pokemon based on a condition
+  //     if (pokemonList[i].height >= 1) {document.write('<p>'+pokemonList[i].name +' '+'(height: '+pokemonList[i].height+') '+'Wow, that\'s a big pokemon!'+'</p>');}
+  //     else {document.write('<p>'+pokemonList[i].name + ' '+'(height: '+pokemonList[i].height+')'+'</p>');}
+  //   }
+
+// calling all pokemon using forEach
+
+function getPokemonList(pokemon)
   {
-    if (pokemonList[i].height >= 1) {document.write('<p>'+pokemonList[i].name +' '+'(height: '+pokemonList[i].height+') '+'Wow, that\'s a big pokemon!'+'</p>');}
-    else {document.write('<p>'+pokemonList[i].name + ' '+'(height: '+pokemonList[i].height+')'+'</p>');}
+    document.write(
+      '<p>'
+      + pokemon.name + '</br>'
+      + 'Height: ' + pokemon.height + '</br>'
+      + 'Type: ' + pokemon.type + '</br>'
+      + 'Ability: ' + pokemon.ability + '</br>'
+      +'</p>');
   }
+
+pokemonList.forEach(getPokemonList);
