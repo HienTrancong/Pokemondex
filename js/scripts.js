6c6c08cad10ff88b759366d363394015bb06e5ec
HienTrancong <89924593+HienTrancong@users.noreply.github.com>
2021-11-25 10:07:03 +0100

    1.7 API
    - fetch pokemon data from API
    - add show details function to log pokemon detail

--- js/scripts.js
+++ js/scripts.js
@@ -2,22 +2,66 @@
 // create pokemon list
 let pokemonRepository = ( // Repository variable to hold what IIFE returns
   function() { //IIFE function to store pokemon list
-    let pokemonList = [
-      {name : 'Bulbasaur', height : 0.7, type : ['grass','poison'], ability : ['chlorophyll','overgrow']},
-      {name : 'Charmander', height : 0.6, type : ['fire'], ability : ['blaze','solar-power']},
-      {name : 'Squirtle', height : 0.5, type : ['water'], ability : ['rain-dish','torrent']},
-      {name : 'Butterfree', height : 1.1, type : ['bug','flying'], ability : ['compoundeyes','tinted-lens']}
-    ];
-    function getAll() {return pokemonList;}
+    let pokemonList = [];
+    let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=100';
+
+    //function to fetch data from pokemon API, as a promise
+    function loadList() {
+      return fetch(apiUrl)
+        .then(function (response){
+          return response.json();
+        })
+        .then(function (json){
+          json.results.forEach(function(item){
+            let pokemon = {name: item.name, detailsUrl: item.url};
+            add(pokemon);
+            console.log(pokemon);
+          })
+        })
+        .catch(function(err){
+          console.error(err);
+        })
+      }
+
+    //function to return all item in pokemon list
+    function getAll() {
+      return pokemonList;}
+
+    //function to add new pokemon in if the input is object and has correct keys
     function add(pokemon) {
       if
-        (typeof(pokemon) !== 'object') // condition to filter only input with object data type
-        {alert('This is not object type');} //statement alert if not object type
+        (typeof(pokemon) !== 'object')
+        {alert('This is not object type');}
       else if
-        ((Object.keys(pokemon)[0] != 'name') || (Object.keys(pokemon)[1] != 'height') || (Object.keys(pokemon)[2] != 'type') || (Object.keys(pokemon)[3] != 'ability')) // condition to filter only correct keys
-        {alert('Not correct keys');} //statement alert if incorrect keys
+        ((Object.keys(pokemon)[0] != 'name') || (Object.keys(pokemon)[1] != 'detailsUrl'))
+        {alert('Not correct keys');}
       else
         (pokemonList.push(pokemon)); //statement to add new pokemon item
+    }
+
+    //function to load detail of each pokemon, taking detailsUrl from function loadList
+    function loadDetails(item) {
+      let url = item.detailsUrl;
+      return fetch(url)
+      .then(function(response){
+        return response.json()
+      })
+      .then(function(details){
+        item.height = details.height;
+        item.types = details.types;
+        item.imageUrl = details.sprites.front_default
+      })
+      .catch(function(err){
+        console.error(err);
+      })
+    }
+
+    //function to take the click event listener trigger to log pokemon
+    function showDetails(item){
+      pokemonRepository.loadDetails(item)
+      .then(function(){
+        console.log(item)
+      });
     }
 
     // function to take each properties from forEach to create button for each pokemon
@@ -38,24 +82,22 @@ let pokemonRepository = ( // Repository variable to hold what IIFE returns
       );
     }
 
-    //function to take the click event listener trigger to log pokemon
-    function showDetails(pokemon) {
-      console.log(pokemon);
-    }
-
     // return getAll function, add function and addListitem functions
     return {
       getAll: getAll,
       add: add,
-      addListItem: addListItem
+      addListItem: addListItem,
+      loadList: loadList,
+      loadDetails: loadDetails,
+      showDetails: showDetails
     }
   }
 )();
 
-//adding 1 pokemon into the list
-pokemonRepository.add({name : 'Pikachu', height : 1, type : ['Electric'], ability : ['static']})
 
 //forEach loop to call each pokemon from getAll function to pass to addListItem function
-pokemonRepository.getAll().forEach( function(pokemon) {
-  pokemonRepository.addListItem(pokemon);}
-);
+pokemonRepository.loadList().then(function(){
+  pokemonRepository.getAll().forEach(function(pokemon) {
+    pokemonRepository.addListItem(pokemon);
+  });
+});
